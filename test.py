a4d1a10c7bc19df849bddcb141bd14b35856a0bf
shreekant <137061697+ScalingGrids@users.noreply.github.com>
2024-05-14 16:22:26 +0530

    Your commit message

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-import keras
-import tensorflow as tf
-import numpy as np
+import subprocess
+import sys
 
-print("Keras version:", keras.__version__)
-print("TensorFlow version:", tf.__version__)
-print("NumPy version:", np.__version__)
+
+def generate_requirements_txt():
+    try:
+        # Run pip freeze command to get installed packages and their versions
+        pip_freeze_output = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze']).decode()
+
+        # Write the packages and their versions to requirements.txt file
+        with open('requirements.txt', 'w') as f:
+            f.write(pip_freeze_output)
+        print("requirements.txt file created successfully.")
+    except Exception as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    generate_requirements_txt()
